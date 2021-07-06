784ba72ce44d224edfed8a68a7310db51a853682
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-07-06 15:35:55 +0200

    Finished GUI

--- next_of_kin.py
+++ next_of_kin.py
@@ -23,29 +23,29 @@ lifechoices_logo_lbl.pack()
 
 # Name
 name_lbl = Label(root, text="Name", font=("Garuda", 15))
-name_lbl.config(bg="#144552", fg="white")
+name_lbl.config(bg="#184E77", fg="#B5E48C")
 name_lbl.place(x=250, y=210)
 
 name_ent = Entry(root)
-name_ent.config(bg="#144552", fg="white")
+name_ent.config(bg="#D9ED92", fg="#184E77")
 name_ent.place(x=350, y=225)
 
 # Phone Number
 phone_lbl = Label(root, text="Phone Number", font=("Garuda", 15))
-phone_lbl.config(bg="#144552", fg="white")
+phone_lbl.config(bg="#184E77", fg="#B5E48C")
 phone_lbl.place(x=175, y=280)
 
 phone_ent = Entry(root)
-phone_ent.config(bg="#144552", fg="white")
+phone_ent.config(bg="#D9ED92", fg="#184E77")
 phone_ent.place(x=350, y=295)
 
 # ID Number
 id_lbl = Label(root, text="ID Number", font=("Garuda", 15))
-id_lbl.config(bg="#144552", fg="white")
+id_lbl.config(bg="#184E77", fg="#B5E48C")
 id_lbl.place(x=215, y=350)
 
 id_ent = Entry(root)
-id_ent.config(bg="#144552", fg="white")
+id_ent.config(bg="#D9ED92", fg="#184E77")
 id_ent.place(x=350, y=365)
 
 
@@ -57,7 +57,7 @@ def confirm():
 
 
 confirm_btn = Button(root, text="Confirm", font=("Garuda", 15), command=confirm)
-confirm_btn.config(bg="#144552", fg="white")
+confirm_btn.config(bg="#D9ED92", fg="#184E77")
 confirm_btn.place(x=40, y=500)
 
 
@@ -68,7 +68,7 @@ def previous():
 
 
 exit_btn = Button(root, text="Exit", font=("Garuda", 15), command=previous)
-exit_btn.config(bg="#144552", fg="white")
+exit_btn.config(bg="#D9ED92", fg="#184E77")
 exit_btn.place(x=600, y=500)
 
 
