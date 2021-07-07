79243f06e4a943f0d50b9471f2f8fff001ae1007
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-07-07 10:54:58 +0200

    Changed GUI according to database

--- next_of_kin.py
+++ next_of_kin.py
@@ -22,31 +22,22 @@ lifechoices_logo_lbl.config(bg="black")
 lifechoices_logo_lbl.pack()
 
 # Name
-name_lbl = Label(root, text="Name", font=("Garuda", 15))
+name_lbl = Label(root, text="Name", font=("Garuda", 20))
 name_lbl.config(bg="#184E77", fg="#B5E48C")
-name_lbl.place(x=250, y=210)
+name_lbl.place(x=315, y=180)
 
 name_ent = Entry(root)
-name_ent.config(bg="#D9ED92", fg="#184E77")
-name_ent.place(x=350, y=225)
+name_ent.config(bg="#D9ED92", fg="#184E77", font=("Garuda", 15))
+name_ent.place(x=240, y=260)
 
 # Phone Number
-phone_lbl = Label(root, text="Phone Number", font=("Garuda", 15))
+phone_lbl = Label(root, text="Phone Number", font=("Garuda", 20))
 phone_lbl.config(bg="#184E77", fg="#B5E48C")
-phone_lbl.place(x=175, y=280)
+phone_lbl.place(x=260, y=340)
 
 phone_ent = Entry(root)
-phone_ent.config(bg="#D9ED92", fg="#184E77")
-phone_ent.place(x=350, y=295)
-
-# ID Number
-id_lbl = Label(root, text="ID Number", font=("Garuda", 15))
-id_lbl.config(bg="#184E77", fg="#B5E48C")
-id_lbl.place(x=215, y=350)
-
-id_ent = Entry(root)
-id_ent.config(bg="#D9ED92", fg="#184E77")
-id_ent.place(x=350, y=365)
+phone_ent.config(bg="#D9ED92", fg="#184E77", font=("Garuda", 15))
+phone_ent.place(x=240, y=430)
 
 
 # Buttons
